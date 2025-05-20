a3bb040ec33eb9e020d6709e52aa97e4da8c8aac
Hoppouu <drone1575@gmail.com>
2025-05-20 12:08:04 +0900

    Add : call() function

--- modules/api_call.py
+++ modules/api_call.py
@@ -56,27 +56,24 @@ def read_srt_file(file_path, fraction = 0.2):
     subtitles = subtitles[:count]
     return subtitles
 
-#자막파일 불러오기
-file_path = 'sample.srt' 
-subtitles = read_srt_file(file_path)
+def call(file_name):
+    #자막파일 불러오기
+    file_path = file_name + '.srt' 
+    subtitles = read_srt_file(file_path)
 
-for subtitle in subtitles:
-    print(subtitle)
+    for subtitle in subtitles:
+        print(subtitle)
 
-#배열의 값을 하나의 문자열로 만들기
-full_text = '\n\n'.join(
-    f"{sub.start} --> {sub.end}\n{sub.text}"
-    for sub in subtitles
-)
-
-print(full_text)
-
-
-
-system_input = """
+    #배열의 값을 하나의 문자열로 만들기
+    full_text = '\n\n'.join(
+        f"{sub.start} --> {sub.end}\n{sub.text}"
+        for sub in subtitles
+    )
+    # print(full_text)
+    system_input = """
     너는 영상 하이라이트 추출기야. 영상은 인터넷 방송이야. 자막 내용을 주면 자막 내용을 보고 영상의 하이라이트라고 생각되는 부분을 타임스탬프와 그 이유와함께 말해줘.
     출력 양식은 [시간대] : [내용]으로 해줘.
-"""
-user_input = full_text
-responses = create_chat_completion(system_input, user_input)
-print(responses.choices[0].message.content)
+    """
+    user_input = full_text
+    responses = create_chat_completion(system_input, user_input)
+    print(responses.choices[0].message.content)
