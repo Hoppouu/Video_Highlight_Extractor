89cb090baacc8cf3e6551416c4f3b913d245e8f5
Hoppouu <drone1575@gmail.com>
2025-06-08 02:21:09 +0900

    Create : constrants.py

---
+++ modules/constants.py
@@ -0,0 +1,6 @@
+file_path_origin = "./result/"
+file_path_subtitle = file_path_origin + "subtitles/"
+file_path_llm_outputs = file_path_origin + "llm_outputs/"
+file_path_images = file_path_origin + "images/"
+
+file_path = [file_path_subtitle, file_path_llm_outputs, file_path_images]
